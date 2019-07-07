bafe780f91267444b2e542b1cb4c17c626bef1c4
Arshad Kazmi <arshadkazmi42@gmail.com>
2019-07-07 19:31:12 +0530

    Add tests

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,40 @@
+const expect = require('chai').expect;
+const fs = require('fs');
+const exec = require('child_process').exec;
+
 const Compose = require('../lib/compose');
 
-const API_FLAG = [ 'api' ];
+const API_FLAG = ['api'];
 
+const isExists = (path) => {
+  try {
+    fs.readFileSync(path, 'utf-8');
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
 
-describe('Verifies async features', function() {
-  it('prints all help flags', function(){
-    Compose(API_FLAG);
+const removeDir = (path) => {
+  return new Promise((resolve) => {
+    exec(`rm -rf ${path}`, (error) => {
+      if (error !== null) {
+        return resolve();
+      }
+
+      return resolve();
+    });
+  });
+};
+
+describe('Verifies async features', function () {
+  it('generates express rest boilerplate', async () => {
+    await Compose(API_FLAG);
+    expect(isExists('express-app/package.json')).to.equal(true);
+    await removeDir('express-app');
+  }).timeout(30000);
+  it('throws error', async () => {
+    await Compose(['dummy']);
+    expect(isExists('express-app/package.json')).to.equal(false);
   });
 });
